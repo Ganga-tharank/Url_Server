eaf29e7eb2b27950e776a4bb7f81b2080ded79c1
Ganga-tharank <154905189+Ganga-tharank@users.noreply.github.com>
2024-07-25 07:13:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config()
 
 const Port = process.env.PORT || 8000;
-const DatabaseAPI = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017'
+const DatabaseAPI = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/'
 
 mongoose.set('strictQuery', false)
 
